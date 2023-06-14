db642517f4137f388d739344691ea17dd41b5129
Lucas Felix <lucariozin.net@gmail.com>
2023-06-13 23:18:43 -0300

    doc: creating the 'Button' component storybook documentation

---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,97 @@
+import type { ComponentProps, ElementType } from 'react'
+
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  boxSizing: 'border-box',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  minWidth: '120px',
+  padding: '0 $4',
+
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+
+  borderRadius: '$sm',
+
+  cursor: 'pointer',
+  transition: '0.2s',
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$ignite500',
+
+        '&:not(:disabled):hover': {
+          background: '$ignite300',
+        },
+
+        '&:disabled': {
+          background: '$gray200',
+        },
+      },
+
+      secondary: {
+        color: '$ignite300',
+        border: '2px solid $ignite500',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+          background: '$ignite500',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+    size: {
+      sm: {
+        height: '38px',
+      },
+
+      md: {
+        height: '46px',
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
